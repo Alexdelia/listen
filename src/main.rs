a993ecf668d6e422523f87386f427214eff3ab69
Alexdelia <alexandre.delille.57@gmail.com>
2024-11-10 11:29:53 +0100

    first test draft

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, Serialize)]
+enum Source {
+    Yt(String),
+    Sc(String, String),
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Entry {
+    s: Source,
+    q: u8,
+}
+
 fn main() {
     println!("Hello, world!");
+
+    let data = ron::from_str::<Vec<Entry>>(
+        r#"[
+    (
+        s: Yt("video_id_123"),
+        q: 10,
+    ),
+    (
+        s: Sc("artist_name", "song_title"),
+        q: 5,
+    ),
+]
+"#,
+    );
+
+    dbg!(data);
 }
