dfdd92e7ac2e1ec372b00fe42b2f998612c49457
Alexdelia <alexandre.delille.57@gmail.com>
2025-02-14 19:08:25 +0100

    chore: `ruff format`

--- graph.py
+++ graph.py
@@ -48,14 +48,17 @@ with timer(os.path.basename(__file__)):
             for datapoint in datapoints:
                 commit, date, timestamp = datapoint.split()
 
-                recording_count = int(
-                    subprocess.run(
-                        f"git show {commit}:{DATA_FILE} | wc -l",
-                        shell=True,
-                        text=True,
-                        capture_output=True,
-                    ).stdout.strip()
-                ) - 2
+                recording_count = (
+                    int(
+                        subprocess.run(
+                            f"git show {commit}:{DATA_FILE} | wc -l",
+                            shell=True,
+                            text=True,
+                            capture_output=True,
+                        ).stdout.strip()
+                    )
+                    - 2
+                )
 
                 if int(timestamp) <= NEW_FORMAT_TIMESTAMP:
                     recording_count //= 5
