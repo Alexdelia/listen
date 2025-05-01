363081f817cfc23c9f9b420121245fc726992f7a
Alexdelia <alexandre.delille.57@gmail.com>
2025-05-01 17:15:18 +0200

    correct recording path in playlist

--- src/playlist/sync.rs
+++ src/playlist/sync.rs
@@ -5,7 +5,7 @@ use hmerr::ioe;
 
 use crate::{
 	channel::{Action, Status},
-	entry::Q,
+	entry::{Entry, Q},
 	filter::SyncEntry,
 };
 
@@ -52,7 +52,7 @@ where
 	let path = path.as_ref();
 
 	let mut set = if path.exists() {
-		parse_content(&fs::read_to_string(path).map_err(|e| ioe!(path.to_string_lossy(), e))?)
+		parse_content(&fs::read_to_string(&path).map_err(|e| ioe!(path.to_string_lossy(), e))?)
 	} else {
 		HashSet::new()
 	};
@@ -64,10 +64,36 @@ where
 		set.remove(&entry);
 	}
 
+	if set.is_empty() {
+		if path.exists() {
+			fs::remove_file(path).map_err(|e| ioe!(path.to_string_lossy(), e))?;
+		}
+		return Ok(());
+	}
+
 	let mut list = set.into_iter().collect::<Vec<_>>();
 	list.sort();
 
-	fs::write(path, list.join("\n")).map_err(|e| ioe!(path.to_string_lossy(), e))?;
+	let recording_path = std::env::current_dir()
+		.map_err(|e| ioe!("current_dir", e))?
+		.join(Entry::OUTPUT_DIR);
+	let recording_path = recording_path
+		.canonicalize()
+		.map_err(|e| ioe!(recording_path.to_string_lossy(), e))?;
+	fs::write(
+		path,
+		list.into_iter()
+			.map(|entry| {
+				recording_path
+					.join(entry)
+					.with_extension(Entry::EXT)
+					.to_string_lossy()
+					.to_string()
+			})
+			.collect::<Vec<_>>()
+			.join("\n"),
+	)
+	.map_err(|e| ioe!(path.to_string_lossy(), e))?;
 
 	Ok(())
 }
