2a3993494622b021d14626249f35f40ca5ef944a
Alexdelia <alexandre.delille.57@gmail.com>
2025-06-13 21:18:05 +0200

    improve dev shell

--- shell.nix
+++ shell.nix
@@ -18,18 +18,22 @@ pkgs.mkShell {
 
   LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [pkgs.openssl];
 
-  shellHook = ''
-    git pull
+  shellHook =
+    /*
+    bash
+    */
+    ''
+      git pull
 
-    # export PATH="$HOME/.cargo/bin:$PATH"
+      # export PATH="$HOME/.cargo/bin:$PATH"
 
-    if [ ! -f .env ]; then
-    	cp .env.example .env
-    	printf "\n\n\t\033[1mplease edit the \033[35m.env\033[39m file\033[0m\n\n"
-    fi
+      if [ ! -f .env ]; then
+      	cp .env.example .env
+      	printf "\n\n\t\033[1mplease edit the \033[35m.env\033[39m file\033[0m\n\n"
+      fi
 
-    alias run='cargo run --release'
+      alias run='cargo run --release'
 
-    alias sync='git add listen.ron && git commit -m "🎶" && git push -q && cargo run --release -q'
-  '';
+      alias sync='git add listen.ron && git commit -m "🎶" && git push -q && cargo run --release -q'
+    '';
 }
