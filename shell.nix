a52772376dd734e5092fd2a1259a73c39d11f12d
Alexdelia <alexandre.delille.57@gmail.com>
2025-09-01 19:35:36 +0200

    direnv support

--- shell.nix
+++ shell.nix
@@ -1,27 +1,35 @@
 {pkgs ? import <nixpkgs> {}, ...}:
 pkgs.mkShell {
-  buildInputs = with pkgs; [
-    openssl
-    pkg-config
-    rust-bin.stable.latest.default
+  buildInputs = with pkgs;
+    [
+      openssl
+      pkg-config
+      rust-bin.stable.latest.default
 
-    python3
-    python3Packages.matplotlib
-    ruff
+      python3
+      python3Packages.matplotlib
+      ruff
 
-    typos
+      typos
 
-    ffmpeg
-    scdl
-    yt-dlp
-  ];
+      ffmpeg
+      scdl
+      yt-dlp
+    ]
+    ++ (
+      let
+        run = "cargo run --release";
+        sync = "git add listen.ron && git commit -m \"🎶\" && git push -q && ${run} -q";
+      in [
+        (pkgs.writers.writeBashBin "run" {} "${run}")
+        (pkgs.writers.writeBashBin "sync" {} "${sync}")
+        (pkgs.writers.writeBashBin "add" {} "$EDITOR listen.ron && ${sync}")
+      ]
+    );
 
   LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [pkgs.openssl];
 
-  shellHook = let
-    run = "cargo run --release";
-    sync = "git add listen.ron && git commit -m \"🎶\" && git push -q && ${run} -q";
-  in
+  shellHook =
     /*
     bash
     */
@@ -34,11 +42,5 @@ pkgs.mkShell {
       	cp .env.example .env
       	printf "\n\n\t\033[1mplease edit the \033[35m.env\033[39m file\033[0m\n\n"
       fi
-
-      alias run='${run}'
-
-      alias sync='${sync}'
-
-      alias add='$EDITOR listen.ron && ${sync}';
     '';
 }
