3cc4429c4cbe49f7e98ea922377f6694a3bc9908
Alexdelia <alexandre.delille.57@gmail.com>
2026-07-16 00:11:12 +0200

    feat: check for duplicate source entry

--- src/declaration/parse.rs
+++ src/declaration/parse.rs
@@ -1,4 +1,4 @@
-use std::{fs, path::Path};
+use std::{collections::HashSet, fs, path::Path};
 
 use hmerr::{ioe, pfe, ple, pwe};
 
@@ -13,7 +13,10 @@ where
 	let content = fs::read_to_string(path).map_err(|e| ioe!(path.to_string_lossy(), e))?;
 
 	match ron::from_str::<Vec<Entry>>(&content) {
-		Ok(list) => Ok(list),
+		Ok(list) => {
+			duplicate(&list, &content, path)?;
+			Ok(list)
+		}
 		Err(e) => {
 			let index = e.span.start.line - 1;
 			let col = e.span.start.col - 1;
@@ -36,4 +39,34 @@ where
 			)?
 		}
 	}
+}
+
+fn duplicate(list: &[Entry], content: &str, path: &Path) -> hmerr::Result<()> {
+	let mut seen = HashSet::with_capacity(list.len());
+
+	let Some(entry) = list.iter().find(|entry| !seen.insert(entry.s)) else {
+		return Ok(());
+	};
+
+	let mbid = entry.s.to_string();
+	let line = content
+		.lines()
+		.enumerate()
+		.filter(|(_, l)| !l.trim_start().starts_with("//") && l.to_lowercase().contains(&mbid))
+		.nth(1)
+		.map(|(index, l)| {
+			ple!(
+				l,
+				i: index,
+				w: pwe!(mbid.clone())
+			)
+		})
+		.unwrap_or_default();
+
+	pfe!(
+		format!("duplicate source {mbid}"),
+		f: path.to_string_lossy(),
+		l: line,
+	)
+	.map_err(Into::into)
 }
